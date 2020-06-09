e68d36298bcd17c8e86b89f3f731c1ada3354441
Shaun Furman <shaunfurman08@gmail.com>
2020-06-08 21:40:52 -0500

    finished repo

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
-// You should NOT change the HTML or CSS in this project (at least until you reach
-// the bonus objectives). Focus on the JavaScript.
+
 
 const findInput = document.querySelector(".find-input")
 const replaceInput = document.querySelector(".replace-input")
 const replaceAllButton = document.querySelector(".replace-all-button")
+   
 
-// The following variable holds your OUTER ARRAY of row elements.
-// Later you will need an OUTER LOOP to loop over the individual elements within
-// this array.
+
 const rowElements = document.querySelectorAll(".row")
-
-// When you call the function belwo, it will get and return an INNER ARRAY
-// containing the cell elements for a given row.
-// Call this function from WITHIN your row elements loop. Then you will, in turn,
-// need to loop over the resulting cell elements. But where should this whole
-// NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
+   
 function getCellElements (currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
 }
 
 
+ document.addEventListener("click", function(){
+        let find = findInput.value ;
+        let replace = replaceInput.value;
+     for (let rowIndex = 0; rowIndex< rowElements.length; rowIndex += 1) {
+        const cellArrays = getCellElements(rowElements[rowIndex])  
+        for (let cellIndex = 0; cellIndex < cellArrays.length; cellIndex += 1){
+            if (cellArrays[cellIndex].innerHTML.includes(find)){
+              const newWord = cellArrays[cellIndex].innerHTML.replace(find, replace)
+              
+              cellArrays[cellIndex].innerHTML = newWord
+              let replacementsMade = newWord.value
+              console.log("replacements Made " + newWord.value)
+            }
+        }  
+    }})
 // YOUR CODE GOES HERE
 
 
